e3454071ca754b1435c334c5e8d1e7fc50726b08
SOFTSERVE\arabosh <arabosh@softserveinc.com>
2023-07-31 19:34:45 +0300

    UC#10 Implement regex

---
+++ RegularExpressionValidationProject/RegularExpressionValidationProject/InputValidation.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegularExpressionValidationProject
+{
+    public class InputValidation
+    {
+        public static bool ValidateInput(string strToValidate, int minLength, int maxLenght, string specialCharacters) 
+        {
+            if (string.IsNullOrEmpty(strToValidate))
+            {
+                return false;
+            }
+
+            var validateGuidRegex = new Regex($"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[{specialCharacters}])(?!.*[\\s]).{{{minLength},{maxLenght}}}$");
+
+            return validateGuidRegex.IsMatch(strToValidate);
+        }
+    }
+}
--- RegularExpressionValidationProject/RegularExpressionValidationProject/Program.cs
+++ RegularExpressionValidationProject/RegularExpressionValidationProject/Program.cs
@@ -2,8 +2,12 @@
 {
     class Program
     {
+        private const string SpecialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
         static void Main(string[] args)
         {
+            var result = InputValidation.ValidateInput("1dD*fghj", 5, 20, SpecialCharacters);
+            Console.WriteLine(result);
         }
     }
 }
